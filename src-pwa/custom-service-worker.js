68de2696a043762152e1ec6af5f592fd36473837
kerasus <aliesmaeeli20@gmail.com>
2023-11-12 10:27:11 +0330

    fix: disable pwa registerRoute

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -9,10 +9,10 @@
 import { clientsClaim } from 'workbox-core'
 import {
   precacheAndRoute,
-  cleanupOutdatedCaches,
-  createHandlerBoundToURL
+  cleanupOutdatedCaches
+  // createHandlerBoundToURL
 } from 'workbox-precaching'
-import { registerRoute, NavigationRoute } from 'workbox-routing'
+// import { registerRoute, NavigationRoute } from 'workbox-routing'
 
 self.skipWaiting()
 clientsClaim()
@@ -22,19 +22,19 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 cleanupOutdatedCaches()
 
-// Non-SSR fallback to index.html
-// Production SSR fallback to offline.html (except for dev)
-if (process.env.MODE !== 'ssr' || process.env.PROD) {
-  registerRoute(
-    new NavigationRoute(
-      createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      {
-        denylist: [
-          /api$/,
-          /sw\.js$/,
-          /workbox-(.)*\.js$/
-        ]
-      }
-    )
-  )
-}
+// // Non-SSR fallback to index.html
+// // Production SSR fallback to offline.html (except for dev)
+// if (process.env.MODE !== 'ssr' || process.env.PROD) {
+//   registerRoute(
+//     new NavigationRoute(
+//       createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
+//       {
+//         denylist: [
+//           /api$/,
+//           /sw\.js$/,
+//           /workbox-(.)*\.js$/
+//         ]
+//       }
+//     )
+//   )
+// }
