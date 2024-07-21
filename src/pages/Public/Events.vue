25f8b483ce7a987f852d2bcf3ae835a71b5d664b
hgh <Haydar.Ghasemi@gmail.com>
2024-07-21 11:11:14 +0330

    اگر کاربر لاگین نبود بخش رویدادهای من و دوره های من نمایش داده نمی شود تا کاربر مجبور به لاگین نشود

--- src/pages/Public/Events.vue
+++ src/pages/Public/Events.vue
@@ -6,9 +6,24 @@
 </template>
 
 <script>
-import { mixinSEO, mixinPageOptions, mixinPrefetchServerData } from 'src/mixin/Mixins.js'
+// import { mixinSEO, mixinPageOptions, mixinPrefetchServerData } from 'src/mixin/Mixins.js'
+import { mixinAuth } from 'src/mixin/Mixins.js'
 export default {
   name: 'Public.AllEvents',
-  mixins: [mixinPrefetchServerData, mixinPageOptions, mixinSEO]
+  mixins: [mixinAuth],
+  // mixins: [mixinPrefetchServerData, mixinPageOptions, mixinSEO]
+  data () {
+    return {
+      pageConfig: {},
+      pageBuilderEditable: false,
+      pageBuilderLoading: false,
+      currenSections: [{ data: { rows: [{ cols: [{ widgets: [{ name: 'AllClassrooms', options: { style: {}, classroomType: 'EVENT' } }], options: {} }], options: { boxed: true } }] } }]
+    }
+  },
+  mounted() {
+    if (this.user && this.user.id !== null) {
+      this.currenSections = [{ data: { rows: [{ cols: [{ widgets: [{ name: 'AllClassrooms', options: { style: {}, classroomType: 'EVENT' } }, { name: 'RegistrationList', options: { style: { marginBottom: '32px' }, classroomType: 'EVENT' } }], options: {} }], options: { boxed: true } }] } }]
+    }
+  }
 }
 </script>
