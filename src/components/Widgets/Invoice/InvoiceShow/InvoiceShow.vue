962b8b70607ae2b3f1fb5c1825e48991756aae7b
HgH <Haydar.Ghasemi@gmail.com>
2024-12-04 16:00:53 +0330

    اضافه کردن شرط جدید برای نمایش لیست اقساط یک صورتحساب
     invoice.status==='PAID_IN_INSTALMENT'

--- src/components/Widgets/Invoice/InvoiceShow/InvoiceShow.vue
+++ src/components/Widgets/Invoice/InvoiceShow/InvoiceShow.vue
@@ -89,13 +89,13 @@
       </q-card-section>
     </q-card>
     <div class="action q-mt-lg">
-      <q-card v-if="!invoice.loading && invoice.status!=='PAYING' && invoice.status!=='PAID_FULL' && invoice.status!=='PAID_IN_INSTALMENT'"
+      <q-card v-if="!invoice.loading && invoice.status!=='PAYING' && invoice.status!=='PAID_FULL'"
               class="q-mb-md">
         <q-card-section>
           تنها در صورتی که وضعیت سفارش در حال پرداخت باشد امکان پرداخت با کیف پول وجود دارد
         </q-card-section>
       </q-card>
-      <q-card v-if="!invoice.loading && (invoice.status==='PAYING' || invoice.status==='PAYING_IN_INSTALMENT' || invoice.status==='PAID_IN_INSTALMENT')">
+      <q-card v-if="!invoice.loading && (invoice.status==='PAYING' || invoice.status==='PAYING_IN_INSTALMENT')">
         <q-card-section>
           پرداخت
         </q-card-section>
