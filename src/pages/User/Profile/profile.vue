4c5495ca087d275c9c149101beb4c70bf37ac8ed
hgh <Haydar.Ghasemi@gmail.com>
2024-12-22 15:37:07 +0330

    وقتی اینپوت
    disable
    
    میشه مقدارش به بکند ارسال نمیشه

--- src/pages/User/Profile/profile.vue
+++ src/pages/User/Profile/profile.vue
@@ -206,7 +206,7 @@ export default {
     isAbroadBirthAddress (newValue) {
       if (!newValue) {
         FormBuilderAssist.setAttributeByName(this.inputs, 'birth_country', 'value', 'ایران')
-        FormBuilderAssist.setAttributeByName(this.inputs, 'birth_country', 'disable', true)
+        // FormBuilderAssist.setAttributeByName(this.inputs, 'birth_country', 'disable', true)
         if (this.inputDataLoaded) {
           // province
           FormBuilderAssist.setAttributeByName(this.inputs, 'birth_province', 'type', 'select')
@@ -240,7 +240,7 @@ export default {
     isAbroadLivingAddress (newValue) {
       if (!newValue) {
         FormBuilderAssist.setAttributeByName(this.inputs, 'living_country', 'value', 'ایران')
-        FormBuilderAssist.setAttributeByName(this.inputs, 'living_country', 'disable', true)
+        // FormBuilderAssist.setAttributeByName(this.inputs, 'living_country', 'disable', true)
         if (this.inputDataLoaded) {
           // province
           FormBuilderAssist.setAttributeByName(this.inputs, 'living_province', 'type', 'select')
