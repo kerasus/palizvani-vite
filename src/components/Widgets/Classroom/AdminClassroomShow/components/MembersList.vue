25f511bfc795a7c1e31d8afa42f12ac9b1933cb0
hgh <haydar.ghasemi@gmail.com>
2025-02-09 11:51:24 +0330

    typo

--- src/components/Widgets/Classroom/AdminClassroomShow/components/MembersList.vue
+++ src/components/Widgets/Classroom/AdminClassroomShow/components/MembersList.vue
@@ -99,7 +99,7 @@ export default {
       exportReportLoading: false,
       membersListInputs: [
         { type: 'hidden', name: 'classroom', value: classroomId },
-        { type: 'input', name: 'owner__national_code', label: 'کد ملی۲', placeholder: ' ', col: 'col-md-3 col-12' },
+        { type: 'input', name: 'owner__national_code', label: 'کد ملی', placeholder: ' ', col: 'col-md-3 col-12' },
         { type: 'select', name: 'status', options: (new Registration()).statusEnums, label: 'وضعیت', placeholder: ' ', col: 'col-md-3 col-12' },
         { type: 'select', name: 'invoice__status', options: (new Invoice()).statusEnums, label: 'نوع صورتحساب', placeholder: ' ', col: 'col-md-3 col-12' },
         { type: BtnControlComp, name: 'btn', label: 'جستجو', placeholder: ' ', atClick: () => {}, col: 'col-md-2 col-12' }
